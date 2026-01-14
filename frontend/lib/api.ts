152a4981855bfdb84f2fd23c5cfb2630c8a1ca1f
tmilinhvtm <tmi.linhvtm@gmail.com>
2026-01-14 11:39:51 +0700

    config env

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,5 @@
 import type { JobFileCategory } from "@/types/shared/job-file";
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
-
-
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 export interface Template {
   id: string;
   fileName: string | null;
